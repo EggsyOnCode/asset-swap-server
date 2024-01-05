ae9af29d2e18089df08307aa2c0b6e6221773783
EggsyOnCode <farzansaqib2003@gmail.com>
2024-01-06 03:45:14 +0500

    user repository added

---
+++ apps/auth/users/user.repository.ts
@@ -0,0 +1,15 @@
+import { BaseAbstractRepository } from '@app/database/abstract.repository';
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.schema';
+
+@Injectable()
+export class UserRepository extends BaseAbstractRepository<User> {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {
+    super(userRepository);
+  }
+}
